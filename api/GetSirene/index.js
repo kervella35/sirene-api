f14ff8848a8f2d8c3ab60d635c5d621dc42973b4
André Kervella <a.kervella@laposte.net>
2021-03-19 20:49:02 +0100

    sirene to siren

--- api/GetSirene/index.js
+++ api/GetSirene/index.js
@@ -1,4 +1,5 @@
 module.exports = async function (context, req) {
+/*
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
@@ -7,7 +8,13 @@ module.exports = async function (context, req) {
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
+        // status: 200, // Defaults to 200 
         body: responseMessage
     };
+*/
+    context.res = {
+        body: {
+            text: "Hello from the API"
+        }
+    }
 }
